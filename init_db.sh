5130d1a35ea77f51ca49917e56369b867c95412f
cs97 <chrswr97@gmail.com>
2024-03-11 16:32:15 +0100

    Update init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -28,14 +28,16 @@ DB_PORT="${POSTGRES_PORT:=5432}"
 
 DB_HOST="${POSTGRES_HOST:=localhost}"
 
-docker run \
-	-e POSTGRES_USER=${DB_USER} \
-	-e POSTGRES_PASSWORD=${DB_PASSWORD} \
-	-e POSTGRES_DB=${DB_NAME} \
-	-p "${DB_PORT}":5432 \
-	-d postgres \
-	postgres -N 1000
-
+# skip docker run
+if [[-z "${SKIP_DOCKER}" ]]; then
+	docker run \
+		-e POSTGRES_USER=${DB_USER} \
+		-e POSTGRES_PASSWORD=${DB_PASSWORD} \
+		-e POSTGRES_DB=${DB_NAME} \
+		-p "${DB_PORT}":5432 \
+		-d postgres \
+		postgres -N 1000
+fi
 
 
 export PGPASSWORD="${DB_PASSWORD}"
