32baa8509229f42fe9d0c252d5b6cc987ef041ed
cs97 <chrswr97@gmail.com>
2024-03-11 03:12:37 +0100

    Update init_db.sh

--- init_db.sh
+++ init_db.sh
@@ -2,6 +2,22 @@
 set -x
 set -eo pipefail
 
+if ! [ -x "$(command -v psql)" ]; then
+	echo >&2 "Errpr: psql is not installed."
+ 	echo >&2 "Use:"
+  	echo >&2 "dnf install postgresql-server postgresql-contrib"
+ 	exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+	echo >&2 "Error:psql is not installed."
+ 	echo >&2 "Use:"
+	echo >&2 "	cargo install --version="~0.6" sqlx-cli --no-default-features --features rustls,postgres"
+ 	echo >&2 "to install it."
+	exit 1
+fi
+
+
 DB_USER=${POSTGRES_USER:=postgres}
 
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
